f887d40543dcf90711715a893acdb060d78185ad
Omri Kalinsky <omri@polycam.ai>
2023-07-19 08:41:13 -0500

    Enabling pretty printing when writing out JSON based GLTFs.

--- open3d/io/file_format/FileGLTF.cpp
+++ open3d/io/file_format/FileGLTF.cpp
@@ -1000,7 +1000,7 @@ bool SaveMeshGLTF(const std::string &fileName, const geometry::TriangleMesh &_me
 
   // setup GLTF
   tinygltf::TinyGLTF gltf;
-  const bool bEmbedImages(bBinary), bEmbedBuffers(bBinary), bPrettyPrint(false);
+  const bool bEmbedImages(bBinary), bEmbedBuffers(bBinary), bPrettyPrint(!bBinary);
   return gltf.WriteGltfSceneToFile(&gltfModel, fileName, bEmbedImages, bEmbedBuffers, bPrettyPrint, bBinary);
 }
 
