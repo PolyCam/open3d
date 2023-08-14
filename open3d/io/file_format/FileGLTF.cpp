6ad65bde4815c89b3ce6cf364877cf35f43caa4c
Omri Kalinsky <omri@polycam.ai>
2023-08-14 17:04:17 -0500

    More logging.

--- open3d/io/file_format/FileGLTF.cpp
+++ open3d/io/file_format/FileGLTF.cpp
@@ -568,14 +568,15 @@ static std::optional<tinygltf::Image> TrySkippedExternalTexture(const geometry::
 
 // export the mesh as a GLTF file
 bool SaveMeshGLTF(const std::string &fileName, const geometry::TriangleMesh &_mesh) {
-  const auto valid_material_id_count = std::count_if(_mesh.triangle_material_ids_.begin(), _mesh.triangle_material_ids_.end(),
-                                                     [](const signed int material_id) { return (material_id >= 0); });
-  const auto negative_one_material_id_count = std::count_if(_mesh.triangle_material_ids_.begin(), _mesh.triangle_material_ids_.end(),
-                                                            [](const signed int material_id) { return (material_id == -1); });
+  auto materials_usage = std::map<signed int, unsigned int>();
+  for (const auto material : _mesh.triangle_material_ids_) {
+    ++materials_usage.insert(std::make_pair(material, 0u)).first->second;
+  }
   std::cout << "open3d::io::SaveMeshGLTF() with " << _mesh.materials_.size() << " materials and " << _mesh.triangle_material_ids_.size()
             << " triangle material ids for " << _mesh.triangles_.size() << " triangles" << std::endl;
-  std::cout << valid_material_id_count << " valid triangle material ids" << std::endl;
-  std::cout << negative_one_material_id_count << " -1 valid triangle material ids" << std::endl;
+  for (const auto &material : materials_usage) {
+    std::cout << "material " << material.first << " used on " << material.second << " triangles" << std::endl;
+  }
 
   const std::string path = utility::filesystem::GetFileParentDirectory(fileName);
   if (!path.empty())
